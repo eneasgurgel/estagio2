26befc101a0041e688742669e9f96ea31b7ad5a9
Gabriel Bezerra <gabrielbrodrigues1@gmail.com>
2022-03-05 19:40:49 -0300

    feat: add express server

---
+++ src/app.ts
@@ -0,0 +1,21 @@
+import express from 'express'
+
+class App {
+    public app : express.Application;
+  
+    constructor() {
+      this.app = express();
+      this.middlewares();
+      this.routes();
+    }
+  
+    middlewares() {
+      this.app.use(express.json());
+    }
+  
+    routes() {
+    }
+  }
+  
+  export default new App().app;
+  
