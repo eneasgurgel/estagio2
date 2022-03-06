8550593b467a9517100e05aa7e031024dff681d9
Gabriel Bezerra <gabrielbrodrigues1@gmail.com>
2022-03-06 17:21:30 -0300

    feat(controllers): add example for controllers and add routes for crud

--- src/app/controllers/wallets.controller.ts
+++ src/app/controllers/wallets.controller.ts
@@ -1,8 +1,35 @@
 import { Request, Response } from 'express';
+import walletsServices from '../services/wallets.services';
 
 class WalletsController {
-    helloworld(req: Request, res: Response) {
-        return res.status(200).json({ message: 'ola mundo' });
+    create(req: Request, res: Response) {
+        const { body } = req;
+        const newWallet = walletsServices.create(body);
+        return res.status(201).json(newWallet);
+    }
+
+    get(req: Request, res: Response) {
+        const allWallets = walletsServices.getAll();
+        return res.status(200).json(allWallets);
+    }
+
+    getOne(req: Request, res: Response) {
+        const { id } = req.params;
+        const oneWallet = walletsServices.getOneId(id);
+        return res.status(200).json(oneWallet);
+    }
+
+    updateOne(req: Request, res: Response) {
+        const { id } = req.params;
+        const { body } = req;
+        const updateWallet = walletsServices.updateOne(id, body);
+        return res.status(200).json(updateWallet);
+    }
+
+    deleteOne(req: Request, res: Response) {
+        const { id } = req.params;
+        walletsServices.deleteOne(id);
+        return res.status(204).end();
     }
 }
 
--- src/routes/wallets.routes.ts
+++ src/routes/wallets.routes.ts
@@ -1,8 +1,12 @@
 import { Application, Router } from 'express';
-import WalletsController from '../app/controllers/wallets.controller';
+import walletsController from '../app/controllers/wallets.controller';
 
 export default async (server: Application, routes: Router, prefix = '/api/v1/wallets') => {
-    routes.get('/', WalletsController.helloworld);
+    routes.post('/', walletsController.create);
+    routes.get('/', walletsController.get);
+    routes.get('/:id', walletsController.getOne);
+    routes.put('/:id', walletsController.updateOne);
+    routes.delete('/:id', walletsController.deleteOne);
 
     server.use(prefix, routes);
 };
