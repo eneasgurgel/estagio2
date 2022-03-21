3ddcb000f3cde927fd40ee983760875a35fd2219
Gabriel Bezerra <gabrielbrodrigues1@gmail.com>
2022-03-20 21:57:16 -0300

    build package

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,10 @@
+import express from 'express';
+import path from 'path';
 import app from './app';
 import 'dotenv/config';
 
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => console.log('server running'));
+
+app.use(express.static(path.resolve(__dirname, './client/build')));
