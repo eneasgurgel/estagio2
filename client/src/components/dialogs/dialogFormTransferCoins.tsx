1d46d428e910a102653da587259bb72c265791f7
Eneas Gurgel <eneasst3241@gmail.com>
2022-05-28 13:30:21 -0300

    Add Icon de perfil, Saque e Transferencia

--- client/src/components/dialogs/dialogFormTransferCoins.tsx
+++ client/src/components/dialogs/dialogFormTransferCoins.tsx
@@ -11,6 +11,10 @@ import AccountBalanceWalletIcon from '@mui/icons-material/AccountBalanceWallet';
 import MenuItem from '@mui/material/MenuItem';
 import CurrencyExchangeIcon from '@mui/icons-material/CurrencyExchange';
 import { useForm } from 'react-hook-form';
+import CoinsService from '../../services/CoinsService';
+import { Alert } from '@mui/material';
+import SuccessAlert from '../alerts/sucessAlert';
+import { PropaneSharp } from '@mui/icons-material';
 
 const currencies = [
     {
@@ -45,47 +49,69 @@ export default function FormDialogTransferCoins() {
     setCurrency(event.target.value);
   };
 
+  const tranferCoin = async ({moeda, amount, address}: any) =>{
+    const data = {
+        coin: moeda,
+        convertFrom: moeda,
+        amount: amount,
+        id: address,
+        type: "transfer"
+    }
+    const teste = await CoinsService.addCoin(data)
+    window.location.reload()
+
+  handleClose()
+
+  return alert('Deposito efetuado com sucesso')
+}
+
   return (
     <div>
-        <Button variant="contained" onClick={handleClickOpen}>Trânsfira moedas</Button>
+        <Button fullWidth variant="contained" onClick={handleClickOpen}>Trânsfira moedas</Button>
       <Dialog open={open} onClose={handleClose}>
         <DialogTitle>Transferênica</DialogTitle>
-        <DialogContent>
-          <DialogContentText>
-            Preencha com o ID da Wallet destino!
-          </DialogContentText>
-        <Box sx={{ display: 'flex', alignItems: 'flex-end' }}>
-            <AccountBalanceWalletIcon sx={{ color: 'action.active', mr: 1, my: 0.5 }} />
-            <TextField id="input-with-sx" label='ID'variant="standard" />
-        </Box>
-        </DialogContent>
-        <DialogContent>
-          <DialogContentText>
-            Preencha com a moeda e o valor da transferência! 
-          </DialogContentText>
-          <TextField
-          id="outlined-select-currency"
-          select
-          value={currency}
-          onChange={handleChange}
-          helperText="Selecione sua moeda"
-          margin="normal"
-        >
-          {currencies.map((option) => (
-            <MenuItem key={option.value} value={option.value}>
-              {option.label}
-            </MenuItem>
-          ))}
-        </TextField>
-        <Box sx={{ display: 'flex', alignItems: 'flex-end' }}>
-        <CurrencyExchangeIcon sx={{ color: 'action.active', mr: 1, my: 0.5 }} />
-        <TextField id="input-with-sx" label={`Valor(${currency})`} variant="standard"  helperText="Selecione o valor" />
-      </Box>
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={handleClose}>Cancelar</Button>
-          <Button onClick={handleClose}>Transferir</Button>
-        </DialogActions>
+        <form onSubmit={handleSubmit(tranferCoin)}>
+            <DialogContent>
+            <DialogContentText>
+                Preencha com o ID da Wallet destino!
+            </DialogContentText>
+            <Box sx={{ display: 'flex', alignItems: 'flex-end' }}>
+                <AccountBalanceWalletIcon sx={{ color: 'action.active', mr: 1, my: 0.5 }} />
+                <TextField id="input-with-sx" label='ID'variant="standard" {...register("address")}/>
+            </Box>
+            </DialogContent>
+            <DialogContent>
+            <DialogContentText>
+                Preencha com a moeda e o valor da transferência! 
+            </DialogContentText>
+            <TextField
+            id="outlined-select-currency"
+            select
+            value={currency}
+            helperText="Selecione sua moeda"
+            margin="normal"
+            {...register("moeda", {
+                onChange: v => setCurrency(v.target.value)
+            })}
+            >
+            {currencies.map((option) => (
+                <MenuItem key={option.value} value={option.value}>
+                {option.label}
+                </MenuItem>
+            ))}
+            </TextField>
+            <Box sx={{ display: 'flex', alignItems: 'flex-end' }}>
+            <CurrencyExchangeIcon sx={{ color: 'action.active', mr: 1, my: 0.5 }} />
+            <TextField id="input-with-sx" label={`Valor(${currency})`} variant="standard"  helperText="Selecione o valor" type="number" {...register("amount", {
+            min: 1
+        })} />
+            </Box>
+            </DialogContent>
+            <DialogActions>
+            <Button onClick={handleClose}>Cancelar</Button>
+            <Button type='submit'>Transferir</Button>
+            </DialogActions>
+        </form>
       </Dialog>
     </div>
   );
--- client/src/components/dialogs/dialogFormWithdrawCoins.tsx
+++ client/src/components/dialogs/dialogFormWithdrawCoins.tsx
@@ -9,6 +9,8 @@ import DialogTitle from '@mui/material/DialogTitle';
 import MenuItem from '@mui/material/MenuItem';
 import CurrencyExchangeIcon from '@mui/icons-material/CurrencyExchange';
 import Box from '@mui/material/Box';
+import { useForm } from 'react-hook-form';
+import CoinsService from '../../services/CoinsService';
 
 const currencies = [
     {
@@ -28,6 +30,7 @@ const currencies = [
 
 export default function FormDialogWithdrawCoins(props: any) {
   const [open, setOpen] = React.useState(false);
+  const { register, handleSubmit } = useForm()
 
   const handleClickOpen = () => {
     setOpen(true);
@@ -44,38 +47,60 @@ export default function FormDialogWithdrawCoins(props: any) {
     setCurrency(event.target.value);
   };
 
+  const withdrawCoins = async ({moeda, amount}: any) =>{
+    const data = {
+        coin: moeda,
+        convertFrom: props.coin,
+        amount: Number(amount),
+        type: "withdraw"
+    }
+    const teste = await CoinsService.addCoin(data)
+    window.location.reload()
+
+  handleClose()
+
+  return alert('Deposito efetuado com sucesso')
+}
+
+
   return (
     <div>
-        <Button variant="contained" onClick={handleClickOpen} color="error">Resgate suas moedas</Button>
+        <Button fullWidth variant="contained" onClick={handleClickOpen} color="error">Resgate suas moedas</Button>
         <Dialog open={open} onClose={handleClose}>
         <DialogTitle>Resgatar</DialogTitle>
-        <DialogContent>
-          <DialogContentText>
-            Preencha as informações para realizar o resgate!
-          </DialogContentText>
-          <TextField
-          id="outlined-select-currency"
-          select
-          value={currency}
-          onChange={handleChange}
-          helperText="Selecione sua moeda"
-          margin="normal"
-        >
-          {currencies.map((option) => (
-            <MenuItem key={option.value} value={option.value}>
-              {option.label}
-            </MenuItem>
-          ))}
-        </TextField>
-      <Box sx={{ display: 'flex', alignItems: 'flex-end' }}>
-        <CurrencyExchangeIcon sx={{ color: 'action.active', mr: 1, my: 0.5 }} />
-        <TextField id="input-with-sx" label={`Valor(${currency})`} variant="standard" />
-      </Box>
+        <form onSubmit={handleSubmit(withdrawCoins)}>
+            <DialogContent>
+            <DialogContentText>
+                Preencha as informações para realizar o resgate!
+            </DialogContentText>
+            <TextField
+            id="outlined-select-currency"
+            select
+            value={currency}
+            helperText="Selecione sua moeda"
+            margin="normal"
+            {...register("moeda", {
+                onChange: v => setCurrency(v.target.value)
+            })}
+            >
+            {currencies.map((option) => (
+                <MenuItem key={option.value} value={option.value}>
+                {option.label}
+                </MenuItem>
+            ))}
+            </TextField>
+        <Box sx={{ display: 'flex', alignItems: 'flex-end' }}>
+            <CurrencyExchangeIcon sx={{ color: 'action.active', mr: 1, my: 0.5 }} />
+            <TextField id="input-with-sx" label={`Valor(${currency})`} variant="standard" type="number" {...register("amount", {
+            min: 1
+        })} />
+        </Box>
         </DialogContent>
         <DialogActions>
-          <Button onClick={handleClose}>Cancelar</Button>
-          <Button onClick={handleClose}>Resgatar</Button>
+            <Button onClick={handleClose}>Cancelar</Button>
+            <Button type='submit'>Resgatar</Button>
         </DialogActions>
+        </form>
       </Dialog>
     </div>
   );
